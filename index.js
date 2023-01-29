4acb1d1c5f96df5bf7a61a19fac21d890e1275e3
Juan SB <juansb.dev@gmail.com>
2023-01-29 19:30:39 -0300

    sigue

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 function clic(){
     let comanda = document.createElement('p');
     let texto = document.getElementById('formu-texto');
+    let inicio = document.getElementById('inicio');
     comanda.innerHTML = texto.value;
     let div1 = document.getElementById('div1');
-    div1.innerHTML = '';
+    comanda.setAttribute('id', 'inicio');
+    inicio.innerHTML = '';
     div1.appendChild(comanda);
     console.log(texto);
    }
