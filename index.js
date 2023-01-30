019148e56f4ff23fe5a83d4e1ddb9026a4b4d518
Juan SB <juansb.dev@gmail.com>
2023-01-30 05:55:39 -0300

    avanza

--- index.js
+++ index.js
@@ -2,12 +2,32 @@
 
 function clic(){
     let comanda = document.createElement('p');
+    comanda.setAttribute('id','p-creado');
     let texto = document.getElementById('formu-texto');
     let inicio = document.getElementById('inicio');
     comanda.innerHTML = texto.value;
     let div1 = document.getElementById('div1');
-    div1.innerHTML = '';
+    div1.innerHTML = "";
     div1.appendChild(comanda);
     console.log(texto);
-   }
+    initAnimation();
+    setTimeout(() => {
+        endAnimation()
+    }, 4000);
+        
+   };
 
+   function initAnimation(){
+    let comandante = document.getElementById('p-creado');
+    comandante.className ='animacion';
+    comandante.style.position='relative'
+   console.log('inicios')
+}
+function endAnimation(){
+    let comandante = document.getElementById('p-creado');
+
+    comandante.className ='comanda1';
+    console.log('finales')
+    comandante.style.position='relative'
+
+}
