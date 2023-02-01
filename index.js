88bc4078c63c0b2d903a7bad161ccabb79c719e2
Juan SB <juansb.dev@gmail.com>
2023-02-01 18:42:03 -0300

    sigue

--- index.js
+++ index.js
@@ -4,26 +4,41 @@ function play() {
     audio.play();}
 
 function clic(){
-    play();
+    //play();
+  
+
+    
     let comanda = document.createElement('p');
     comanda.setAttribute('id','p-creado');
     let texto = document.getElementById('formu-texto');
     if (texto.value < 999 ) {
         comanda.innerHTML = texto.value;
-    let div1 = document.getElementById('div1');
-    div1.innerHTML = "";
-    div1.appendChild(comanda);
-    console.log(texto);
-    initAnimation();
+        guarda();
+        let div1 = document.getElementById('div1');
+        div1.innerHTML = "";
+        div1.appendChild(comanda);
+        
+
+        let comanda2 = document.createElement('p');
+    comanda2.setAttribute('id','p-creado2');
+    let anterior = document.getElementById('comanda-ant');
+    let comanda1 = document.getElementById('p-creado');
+        console.log(texto.value);
+        initAnimation();
     setTimeout(() => {
         endAnimation()
     }, 7000);
-    } else {
-        alert('Solo admite 3 cifras')
-    }
-    
+} else {
+    alert('Solo admite 3 cifras')
+}
+
         
    };
+   function guarda(){
+    let guardado = document.getElementById('p-creado');
+    guardado.innerHTML = comanda;
+    console.log(guardado)
+   }
 
    function initAnimation(){
     let comandante = document.getElementById('p-creado');
